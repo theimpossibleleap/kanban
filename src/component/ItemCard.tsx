9825f018513e0d439d967adfcdeb405a15b78859
Adam Punjabi <122632859+theimpossibleleap@users.noreply.github.com>
2025-09-26 15:56:39 -0700

    Adds drop shadows and makes cards/dropdowns look better

--- src/component/ItemCard.tsx
+++ src/component/ItemCard.tsx
@@ -10,16 +10,28 @@ const ItemCard: React.FC<ItemProps> = ({ item, setTaskData }) => {
     <>
       <div
         style={{
+          display: "flex",
+          flexDirection: "column",
           marginBottom: "3em",
           background: "white",
           border: "1px solid rgba(0,0,0,0.1)",
           margin: "1em",
           padding: "1em",
           borderRadius: "1em",
+          boxShadow: "0px 0px 15px rgba(0,0,0,0.1)",
+          textAlign: "left",
         }}
       >
         <p>{item.item}</p>
         <select
+          style={{
+            all: "unset",
+            padding: "0.5em",
+            fontSize: "0.75em",
+            background: "#f1f1f1",
+            borderRadius: "5px",
+            border: "1px solid rgba(0,0,0,0.1)"
+          }}
           value={item.status}
           onChange={(e) =>
             setTaskData((prev) =>
